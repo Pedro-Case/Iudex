f73a77ba69333fc4a8292d2be0424d728543fe11
Pedro Vasconcelos de Menezes Case <pvmc@windows.cin.ufpe.br>
2023-11-16 10:03:29 -0300

    Q1

--- L1Q1.py
+++ L1Q1.py
@@ -5,35 +5,38 @@ class Pilha:
         self.tamanho = 0
 
 class Caixa:
-    def __init__(self, valor, paridade):
+    def __init__(self, valor):
         self.proximo = None
         self.anterior = None
         self.valor = valor
-        self.paridade = paridade # 1 é impar 0 é par
 def main():
     n_de_pilhas = int(input())
     for _ in range(1, n_de_pilhas + 1):
         pilha_caixas = Pilha()
-        entrada = int(input())
+        entrada = input()
         while entrada != 0:
-            paridade = entrada % 2 
+            entrada = int(input()) 
             if pilha_caixas.base == None:
-                caixa = Caixa(entrada, paridade)
+                caixa = Caixa(entrada)
                 pilha_caixas.base = caixa
                 pilha_caixas.topo = caixa
                 pilha_caixas.tamanho += 1
             else:
-                # Caixa é adicionada normalmente
-                caixa = Caixa(entrada, paridade)
-                pilha_caixas.topo.proximo = caixa
-                caixa.anterior = pilha_caixas.topo
-                pilha_caixas.topo = caixa
-                pilha_caixas.tamanho += 1
-            entrada = int(input())
+                caixa = Caixa(entrada)
+                if caixa.valor % 2 == pilha_caixas.topo.valor % 2:
+                    if caixa.valor > pilha_caixas.topo.valor:
+                            diferenca = caixa.valor - pilha_caixas.topo.valor
+                    else:
+                        diferenca = pilha_caixas.topo.valor - caixa.valor
+                    pilha_caixas.topo.valor = diferenca
+                else:
+                    pilha_caixas.topo.proximo = caixa
+                    caixa.anterior = pilha_caixas.topo
+                    pilha_caixas.topo = caixa
+                    pilha_caixas.tamanho += 1
     termo_acessado = pilha_caixas.topo
     while termo_acessado.anterior != None:
         if termo_acessado.paridade == termo_acessado.anterior.paridade == 0:
-        termo_acessado = termo_acessado.anterior
 
 
 if __name__ == '__main__':
