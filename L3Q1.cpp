2c7d1a86824e26a766c61738c9d984be9ed4f5b9
Pedro Vasconcelos de Menezes Case <pvmc@WINDOWS.CIN.UFPE.BR>
2023-12-12 08:36:57 -0300

    L3

---
+++ L3Q1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+struct Objeto
+{
+    string Nome;
+    Objeto * prox;
+};
+
+
+int main(int argc, char *argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tamanho, n_nomes, n_entradas;
+    string entrada, argumento;
+    cin >> tamanho, n_nomes;
+    for(int i = 0; i++; i < n_nomes){
+        cin >> entrada, argumento;
+    }
+    cin >> n_entradas;
+    for(int i = 0; i++; i < n_entradas){
+        cin >> entrada, argumento;
+    }
+    return 0;
+}
