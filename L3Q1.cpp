c4900565f5d9a2330f0be54001e1ae20ed934ab9
Pedro Vasconcelos de Menezes Case <pvmc@WINDOWS.CIN.UFPE.BR>
2023-12-12 09:56:03 -0300

    lista 3

--- L3Q1.cpp
+++ L3Q1.cpp
@@ -3,10 +3,18 @@
 using namespace std;
 struct Objeto
 {
-    string Nome;
+    string nome;
     Objeto * prox;
 };
 
+int hash_funct(string nome, int tamanho){
+    int soma = 0;
+    for(int c; nome[c]; c++){
+        soma = soma + (int(nome[c])*c);
+    }
+    soma = (soma * 17) % tamanho;
+    return soma;
+}
 
 int main(int argc, char *argv[]) {
     ios::sync_with_stdio(false);
@@ -15,8 +23,12 @@ int main(int argc, char *argv[]) {
     int tamanho, n_nomes, n_entradas;
     string entrada, argumento;
     cin >> tamanho, n_nomes;
+    string tabela[tamanho];
     for(int i = 0; i++; i < n_nomes){
         cin >> entrada, argumento;
+        Objeto pessoa;
+        pessoa.nome = entrada;
+        tabela[hash_funct(pessoa.nome, tamanho)];
     }
     cin >> n_entradas;
     for(int i = 0; i++; i < n_entradas){
